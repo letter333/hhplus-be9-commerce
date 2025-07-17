074858cc1ae230105ebaa6e9366bdbf6fad7acf6
letter333 <seryong12@gmail.com>
2025-07-17 22:59:33 +0900

    feat: 쿠폰 조회 Mock API

---
+++ src/main/java/kr/hhplus/be/server/mock/coupon/MockCouponController.java
@@ -0,0 +1,99 @@
+package kr.hhplus.be.server.mock.coupon;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.hhplus.be.server.mock.common.Response;
+import kr.hhplus.be.server.mock.coupon.dto.CouponResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Tag(name = "Mock Coupon API", description = "Coupon 발급, 조회")
+public class MockCouponController {
+    @Operation(summary = "쿠폰 조회", description = "사용자 ID를 이용해 쿠폰 조회")
+    @ApiResponse(
+            responseCode = "200",
+            description = "쿠폰 조회 성공",
+            content = @Content(
+                    schema = @Schema(implementation = Response.class),
+                    examples = @ExampleObject(
+                            name = "쿠폰 조회 예시",
+                            value = """
+                                    {
+                                      "code": "OK",
+                                      "message": null,
+                                      "data": [
+                                        {
+                                          "id": 1,
+                                          "couponName": "신규 가입 쿠폰",
+                                          "couponCode": "abcdef",
+                                          "couponType": "AMOUNT",
+                                          "amount": 10000,
+                                          "percentage": null,
+                                          "status": "ISSUED",
+                                          "usedAt": null,
+                                          "expiredAt": "2025-07-31T00:00:00+09:00",
+                                          "createdAt": null
+                                        },
+                                        {
+                                          "id": 2,
+                                          "couponName": "선착순 할인 쿠폰",
+                                          "couponCode": "1234512345",
+                                          "couponType": "AMOUNT",
+                                          "amount": 5000,
+                                          "percentage": null,
+                                          "status": "ISSUED",
+                                          "usedAt": null,
+                                          "expiredAt": "2025-07-31T00:00:00+09:00",
+                                          "createdAt": null
+                                        }
+                                      ]
+                                    }
+                """
+                    )
+            )
+    )
+    @GetMapping(value = "/api/v1/users/{id}/coupons")
+    public Response<List<CouponResponse>> getCouponList(
+            @Parameter(description = "사용자 ID", example = "1") @PathVariable Long id
+    ) {
+        List<CouponResponse> couponResponses = new ArrayList<>();
+
+        couponResponses.add(
+                new CouponResponse(
+                        1L,
+                        "신규 가입 쿠폰",
+                        "abcdef", CouponResponse.type.AMOUNT,
+                        10000L,
+                        null,
+                        CouponResponse.status.ISSUED,
+                        null,
+                        ZonedDateTime.of(2025, 7, 31, 0, 0, 0, 0, ZoneId.of("Asia/Seoul")),
+                        null)
+        );
+
+        couponResponses.add(
+                new CouponResponse(
+                        2L,
+                        "선착순 할인 쿠폰",
+                        "1234512345", CouponResponse.type.AMOUNT,
+                        5000L,
+                        null,
+                        CouponResponse.status.ISSUED,
+                        null,
+                        ZonedDateTime.of(2025, 7, 31, 0, 0, 0, 0, ZoneId.of("Asia/Seoul")),
+                        null)
+        );
+
+        return Response.ok(couponResponses);
+    }
+}
