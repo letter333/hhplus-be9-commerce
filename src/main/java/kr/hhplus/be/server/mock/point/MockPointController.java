08c39658d4d01e21f0b9a1840040a750096e680a
letter33 <seryong12@gmail.com>
2025-07-25 09:57:48 +0900

    refactor: 기존 MockAPI 주석 처리

--- src/main/java/kr/hhplus/be/server/mock/point/MockPointController.java
+++ src/main/java/kr/hhplus/be/server/mock/point/MockPointController.java
@@ -16,31 +16,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Tag(name = "Mock Point API", description = "Point 충전, 조회")
 public class MockPointController {
-    @Operation(summary = "포인트 조회", description = "사용자 ID를 이용해 포인트 조회")
-    @ApiResponse(
-            responseCode = "200",
-            description = "충전 성공",
-            content = @Content(
-                    schema = @Schema(implementation = Response.class),
-                    examples = @ExampleObject(
-                            name = "포인트 조회 예시",
-                            value = """
-                {
-                    "code": "OK",
-                    "message": "포인트 조회 성공",
-                    "data": {
-                        "id": 1,
-                        "amount": 100000
-                    }
-                }
-                """
-                    )
-            )
-    )
-    @GetMapping(value = "/api/v1/users/{id}/points")
-    public Response<PointResponse> getPoint(@Parameter(description = "사용자 ID", example = "1") @PathVariable Long id) {
-        return Response.ok("포인트 조회 성공", new PointResponse(1L, 100000L));
-    }
+//    @Operation(summary = "포인트 조회", description = "사용자 ID를 이용해 포인트 조회")
+//    @ApiResponse(
+//            responseCode = "200",
+//            description = "충전 성공",
+//            content = @Content(
+//                    schema = @Schema(implementation = Response.class),
+//                    examples = @ExampleObject(
+//                            name = "포인트 조회 예시",
+//                            value = """
+//                {
+//                    "code": "OK",
+//                    "message": "포인트 조회 성공",
+//                    "data": {
+//                        "id": 1,
+//                        "amount": 100000
+//                    }
+//                }
+//                """
+//                    )
+//            )
+//    )
+//    @GetMapping(value = "/api/v1/users/{id}/points")
+//    public Response<PointResponse> getPoint(@Parameter(description = "사용자 ID", example = "1") @PathVariable Long id) {
+//        return Response.ok("포인트 조회 성공", new PointResponse(1L, 100000L));
+//    }
 
 //    @Operation(summary = "잔액 충전", description = "사용자 ID를 이용해 잔액 충전")
 //    @ApiResponse(
