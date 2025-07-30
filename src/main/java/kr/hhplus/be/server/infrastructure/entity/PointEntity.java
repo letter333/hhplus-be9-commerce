3f57089fdaa97a4c2f7c887351cb4337cb222a9f
letter333 <seryong12@gmail.com>
2025-07-30 23:35:01 +0900

    refactor: PointEntity Entity관련 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/infrastructure/entity/PointEntity.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/entity/PointEntity.java
@@ -1,12 +1,21 @@
 package kr.hhplus.be.server.infrastructure.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "point")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PointEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
     private Long userId;
+
+    @Column(nullable = false)
     private Long balance;
 
     @Builder
