b57e318daab70037344d8c9989aff34a51c289e6
letter333 <seryong12@gmail.com>
2025-07-30 23:52:01 +0900

    refactor: ProductEntity Entity관련 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/infrastructure/entity/ProductEntity.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/entity/ProductEntity.java
@@ -1,17 +1,30 @@
 package kr.hhplus.be.server.infrastructure.entity;
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "product")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
     private Long price;
+
+    @Column(nullable = false)
     private int stock;
 
     @Builder
