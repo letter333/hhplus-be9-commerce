f16e4683adbf18af61980c3a25ade1b5f1eca260
letter333 <seryong12@gmail.com>
2025-08-01 01:26:39 +0900

    refactor: Order Jpa 적용

--- src/main/java/kr/hhplus/be/server/infrastructure/repository/jpa/order/OrderJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/repository/jpa/order/OrderJpaRepository.java
@@ -1,12 +1,21 @@
 package kr.hhplus.be.server.infrastructure.repository.jpa.order;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.model.OrderStatus;
 import kr.hhplus.be.server.infrastructure.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
     Optional<OrderEntity> findByStatusAndCreatedAtBefore(OrderStatus status, LocalDateTime dateTime);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select o from OrderEntity o where o.id = :id")
+    Optional<OrderEntity> findByIdWithLock(@Param("id") Long id);
+
 }
