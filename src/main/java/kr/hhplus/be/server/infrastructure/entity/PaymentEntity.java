603ff869b57b4ed8760608b74517f1de9a41da3c
letter333 <seryong12@gmail.com>
2025-08-01 01:20:34 +0900

    refactor: Payment Jpa 사용으로 변경

--- src/main/java/kr/hhplus/be/server/infrastructure/entity/PaymentEntity.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/entity/PaymentEntity.java
@@ -1,16 +1,33 @@
 package kr.hhplus.be.server.infrastructure.entity;
 
+import jakarta.persistence.*;
 import kr.hhplus.be.server.domain.model.PaymentMethod;
 import kr.hhplus.be.server.domain.model.PaymentStatus;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "payments")
 @Getter
+@NoArgsConstructor
 public class PaymentEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Long orderId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private PaymentMethod paymentMethod;
+
+    @Column(nullable = false)
     private Long amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private PaymentStatus status;
 
     @Builder
