aa3cb91e54e2e7fca79a32a1ccbab3caa87b7b45
letter333 <seryong12@gmail.com>
2025-08-01 01:01:24 +0900

    refactor: Valid 추가

--- src/main/java/kr/hhplus/be/server/interfaces/dto/request/PaymentProcessRequest.java
+++ src/main/java/kr/hhplus/be/server/interfaces/dto/request/PaymentProcessRequest.java
@@ -1,9 +1,14 @@
 package kr.hhplus.be.server.interfaces.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kr.hhplus.be.server.domain.model.PaymentMethod;
 
 public record PaymentProcessRequest(
+        @NotNull(message = "주문 ID는 필수입니다.")
         Long orderId,
+
+        @NotNull(message = "결제 방법은 필수입니다.")
         PaymentMethod paymentMethod
 ) {
 }
