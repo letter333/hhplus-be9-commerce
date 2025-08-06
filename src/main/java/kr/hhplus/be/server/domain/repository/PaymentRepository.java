2b65a1fbec3e8d9b7c9819048e6bee3c55b15dcd
letter333 <68866597+letter333@users.noreply.github.com>
2025-08-06 14:48:19 +0900

    Update PaymentRepository.java

--- src/main/java/kr/hhplus/be/server/domain/repository/PaymentRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/repository/PaymentRepository.java
@@ -2,6 +2,9 @@ package kr.hhplus.be.server.domain.repository;
 
 import kr.hhplus.be.server.domain.model.Payment;
 
+import java.util.List;
+
 public interface PaymentRepository {
     Payment save(Payment payment);
+    List<Payment> saveAll(List<Payment> payments);
 }
