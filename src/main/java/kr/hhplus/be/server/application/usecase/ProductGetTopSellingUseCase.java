246f82109e3aa8620514ddc8acfe18af19badc1a
letter333 <seryong12@gmail.com>
2025-08-15 13:12:30 +0900

    refactor: 인기 상품 조회 캐시 적용

--- src/main/java/kr/hhplus/be/server/application/usecase/ProductGetTopSellingUseCase.java
+++ src/main/java/kr/hhplus/be/server/application/usecase/ProductGetTopSellingUseCase.java
@@ -5,12 +5,16 @@ import kr.hhplus.be.server.domain.model.Product;
 import kr.hhplus.be.server.domain.repository.ProductRepository;
 import kr.hhplus.be.server.infrastructure.repository.jpa.product.ProductJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductGetTopSellingUseCase {
@@ -18,7 +22,12 @@ public class ProductGetTopSellingUseCase {
     private static final Long POPULAR_LIMIT = 5L;
     private static final int RECENT_DAYS = 3;
 
+    @Cacheable(value = "topSellingProducts",
+            key = "'topSellingProducts:' + T(java.time.LocalDate).now().toString()",
+            unless = "#result.isEmpty()")
+    @Transactional(readOnly = true)
     public List<TopSellingProduct> execute() {
+        log.info("-------------------캐시 처리 안됨");
         LocalDateTime threeDaysAgoAtMidnight = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS).minusDays(RECENT_DAYS);
 
         List<ProductJpaRepository.TopSellingProductView> topProducts = productRepository.findTopSellingProducts(threeDaysAgoAtMidnight, POPULAR_LIMIT);
--- src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
@@ -3,11 +3,19 @@ package kr.hhplus.be.server.config.redis;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RedissonConfig {
@@ -25,5 +33,24 @@ public class RedissonConfig {
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
         return Redisson.create(config);
+    }
+
+    @Bean
+    CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<>();
+        config.put("topSellingProducts", new CacheConfig(
+                calculateTTLUntilMidnight(),
+                TimeUnit.MINUTES.toMillis(30)
+        ));
+        return new RedissonSpringCacheManager(redissonClient, config);
+    }
+
+    private long calculateTTLUntilMidnight() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nextMidnight = LocalDate.now().plusDays(1).atStartOfDay();
+
+        long secondsUntilMidnight = java.time.Duration.between(now, nextMidnight).getSeconds();
+
+        return secondsUntilMidnight * 1000;
     }
 }
