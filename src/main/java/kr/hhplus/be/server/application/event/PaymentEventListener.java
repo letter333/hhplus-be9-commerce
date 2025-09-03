acc43af15ce1b003a36008b1b03870777da9e1d5
letter333 <68866597+letter333@users.noreply.github.com>
2025-09-03 14:14:59 +0900

    STEP15 (#10)
    * refactor: 외부 데이터 플랫폼으로 결제 정보 전송 이벤트 재시도 추가
    
    * 이벤트 적용 커밋
    
    * docs: 서비스 확장 설계 문서 작성

--- src/main/java/kr/hhplus/be/server/application/event/PaymentEventListener.java
+++ src/main/java/kr/hhplus/be/server/application/event/PaymentEventListener.java
@@ -3,10 +3,14 @@ package kr.hhplus.be.server.application.event;
 import kr.hhplus.be.server.domain.model.Payment;
 import kr.hhplus.be.server.domain.service.ExternalPaymentDataPlatformService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PaymentEventListener {
@@ -14,12 +18,17 @@ public class PaymentEventListener {
 
     @Async
     @TransactionalEventListener
+    @Retryable(
+            value = Exception.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 100, multiplier = 2.0)
+    )
     public void handlePaymentSuccessEvent(PaymentSuccessEvent event) {
         Payment payment = event.payment();
         try {
             externalPaymentDataPlatformService.sendPaymentInfo(payment);
         } catch (Exception e) {
-
+            log.warn("외부 데이터 플랫폼으로 결제 정보 전송 실패 paymentId : {}, error : {}", payment.getId(), e.getMessage());
         }
     }
 }
